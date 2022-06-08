4cb6170da5bb4c47fe0f2751cf87bc2eb5f10bc6
icecoldsprite1 <67610746+icecoldsprite1@users.noreply.github.com>
2022-06-08 04:02:22 -0700

    borders

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-const ACCELERATION = 500
-const MAX_SPEED = 120
-const FRICTION = 500
+const ACCELERATION = 1000
+const MAX_SPEED = 300
+const FRICTION = 1000
 
 var velocity = Vector2.ZERO
 
